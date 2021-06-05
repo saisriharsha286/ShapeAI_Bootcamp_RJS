01aa9ea241b07e6140bd251cf1b89a32099b1e16
saisriharsha286 <85404999+saisriharsha286@users.noreply.github.com>
2021-06-06 00:42:28 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js </h1>
+      <p>
+        This was an amazing Bootcame as it was very knowledgable and heplful for
+        me. If you want to learn complete WebD then please join the program.
+        Thank you...
+      </p>
+    </div>
+  );
+}
+
+export default Note;
